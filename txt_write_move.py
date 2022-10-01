0dbcc566c3b9b522e46318fed5ec5a26dd7c59d9
Ignar <Ignarvalme@gmail.com>
2022-10-01 12:18:00 +0300

    nr update

--- txt_write_move.py
+++ txt_write_move.py
@@ -11,10 +11,11 @@ class Logger(object):
         self.log.write(message)
 
     def flush(self):
-        #this flush method is needed for python 3 compatibility.
-        #this handles the flush command by doing nothing.
-        #you might want to specify some extra behavior here.
+        # this flush method is needed for python 3 compatibility.
+        # this handles the flush command by doing nothing.
+        # you might want to specify some extra behavior here.
         pass
 
+
 '# Call out class so it runs when imported'
-#sys.stdout = Logger()
+# sys.stdout = Logger()
