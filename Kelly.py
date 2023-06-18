639b5fdcff028413b8be1434dc13f28cd026a5e0
Ignar <Ignarvalme@gmail.com>
2023-06-18 18:37:36 +0300

    nr update

--- Kelly.py
+++ Kelly.py
@@ -3,7 +3,7 @@ import Aktsiad
 jur_eur_stocks = {
                 "EGR1T": 172,
                 "HPR1T": 23,
-                "EXXT": 25.162,
+                "EXXT": 26.485,
                 "MAGIC.RG": 35
                 }
 
@@ -12,7 +12,7 @@ ETH_amount = 0.10581
 ETH_EUR = Aktsiad.crypto_in_eur('Ethereum') * ETH_amount
 
 Kelly_raha = 0
-Kelly_Invest_raha = 180
+Kelly_Invest_raha = 0
 Kelly_Invest_aktsiad = Aktsiad.stocks_value_combined(stock_dictionary=jur_eur_stocks, org_currency=True)
 
 Kelly_Portfell_Kokku = round(Kelly_raha + Kelly_Invest_raha + ETH_EUR + Kelly_Invest_aktsiad)
--- Morr.py
+++ Morr.py
@@ -3,23 +3,21 @@ import Aktsiad
 morr_eur_stocks = {"EFT1T": 55,
                    "TKM1T": 53,
                    "TSM1T": 560,
-                   "EXS1": 0.013,
-                   "EXSA.DE": 0.069,
-                   "EXXT": 41.794,
-                   "SPYW": 182.854
+                   "EXXT": 42.434,
+                   "SPYW": 184.498
                    }
 
 '''morr_usa_stocks = {}'''
 
 '''Sõle_Laen_Kuupäev = date(2011, 8, 25) #Müüdud 22.06.2021'''
 
-ValCapitalRaha = 12040
+ValCapitalRaha = 12260
 
-Lähtse_Raha = 110420
+Lähtse_Raha = 117382
 
 
 m_aktsiad = round(Aktsiad.stocks_value_combined(stock_dictionary=morr_eur_stocks, org_currency=True))
 
-m_raha = 133.96
+m_raha = 0
 
 kokku = round(ValCapitalRaha / 2 + m_raha + m_aktsiad + Lähtse_Raha / 2)
